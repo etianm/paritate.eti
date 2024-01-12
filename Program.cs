121fc4d8bdf4111b338cb2b9631ba88e7ff647cf
etianm <155981372+etianm@users.noreply.github.com>
2024-01-13 00:41:08 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int a;
+        a = Convert.ToInt32(Console.ReadLine());
+        if (a % 2 == 0)
+            Console.WriteLine("PAR");
+        else
+            Console.WriteLine("IMPAR");
+    }
+}
